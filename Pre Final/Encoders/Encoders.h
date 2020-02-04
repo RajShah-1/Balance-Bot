fcef31f0a98b7fe3f13d48efb888f381c08fd58e
Shivam Malviya <46934893+shivammalviya712@users.noreply.github.com>
2020-02-05 02:21:29 +0530

    Work of 4 Feb

---
+++ Pre Final/Encoders/Encoders.h
@@ -0,0 +1,32 @@
+/*
+	Encoders.h - A Library for Encoders
+	Created by Shivam Malviya, Jan 30, 2020.
+*/
+#ifndef Encoders_h
+#define Encoders_h
+
+#include "Arduino.h"
+
+#define RADIUS 0.03
+#define RESOLUTION 540
+
+class Encoders {
+
+    public:
+        byte A;
+        byte B;
+        volatile byte lastStateA;
+        volatile int rotation;
+        volatile double phiDot;
+        volatile unsigned long timeStamp;
+
+    public:
+        Encoders(byte A=0, byte B=0);
+        void init();
+        double getPhi();
+        double getPhiDot();
+        double getX();
+        double getXDot();
+};
+
+#endif
