afa544545835c5221b74f6b508c5ac72e9eb8545
RajShah-1 <srk13@iitbbs.ac.in>
2020-03-03 17:24:41 +0530

    Deleted older versions of libraries and sketches also deleted unnecessary simulations

--- Octave/Cost2.m
+++
@@ -1,8 +0,0 @@
-function cost, Q = Cost2(data)
-    x = data(:, 1);
-    xDot = data(:, 2);
-    theta = data(:, 3);
-    thetaDot = data(:, 4);
-    torque = data(:, 5);
-    cost = 10*x'*x + 10*xDot'*xDot + 10*theta'*theta + 5*thetaDot'*thetaDot + 2*torque*torque;
-endfunction
--- Octave/add.m
+++
@@ -1,3 +0,0 @@
-function Q = add(Q, i)
-  Q(i, i) += 0.01;
-endfunction
